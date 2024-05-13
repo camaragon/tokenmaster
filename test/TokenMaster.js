04ecad82e2ee600ceedd3b39b94914472786a975
Cameron Aragon <caragon4695@gmail.com>
2024-05-12 22:14:52 -0600

    setup on starter branch

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,34 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+const NAME = "TokenMaster";
+const SYMBOL = "TM";
 
 describe("TokenMaster", () => {
+  let tokenMaster;
+  let deployer, buyer;
 
-})
+  beforeEach(async () => {
+    //Setup accounts
+    [deployer, buyer] = await ethers.getSigners();
+
+    const TokenMaster = await ethers.getContractFactory(NAME);
+    tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
+  });
+
+  describe("Deployment", () => {
+    it("sets the name", async () => {
+      const name = await tokenMaster.name();
+      expect(name).to.equal(NAME);
+    });
+
+    it("sets the name", async () => {
+      const symbol = await tokenMaster.symbol();
+      expect(symbol).to.equal(SYMBOL);
+    });
+
+    it("sets the owner", async () => {
+      expect(await tokenMaster.owner()).to.equal(deployer.address);
+    });
+  });
+});
